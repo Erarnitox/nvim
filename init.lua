e4304e791d8583c5ff400c20acccc86dfcbdb178
Erarnitox <david@erarnitox.de>
2023-11-23 00:42:07 +0100

    update plugins

--- init.lua
+++ init.lua
@@ -181,6 +181,25 @@ require('lazy').setup({
     },
     build = ':TSUpdate',
   },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
+
+      -- optional
+      "nvim-tree/nvim-web-devicons",
+
+      -- recommended
+      -- "rcarriga/nvim-notify",
+    },
+    opts = {
+      -- configuration goes here
+    },
+  },
 }, {})
 
 -- [[ Setting options ]]
